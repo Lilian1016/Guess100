0ebc3ce0f660ace64260be47e1bcca6390a51843
lilian1016 <bone861016@gmail.com>
2017-11-14 10:41:50 +0800

    Guess

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.user.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R .layout.activity_main);
-    }
-    public void findViews(View view){
-        EditText secret =(EditText)findViewById(R.id.secret);
-        EditText info =(EditText)findViewById(R.id.info);
-        EditText numbeer =(EditText)findViewById(R.id.number);
-        EditText send =(EditText)findViewById(R.id.send);
-        setContentView(R.layout.activity_main);
 
     }
-}
+    public void findViews(View view) {
+        EditText secret = (EditText) findViewById(R.id.secret);
+        EditText info = (EditText) findViewById(R.id.info);
+        EditText number = (EditText) findViewById(R.id.number);
+        EditText send = (EditText) findViewById(R.id.send);
+        setContentView(R.layout.activity_main);
+        int x = Integer.parseInt(number.getText().toString() );
+                Random random = new Random();
+        int y = random.nextInt(100) + 1;
+
+     }
+    }
+
+
+
+
+

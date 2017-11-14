82073cecd4f04bb8b3e9fce815af27bee29a9c15
lilian1016 <bone861016@gmail.com>
2017-11-14 09:55:21 +0800

    findViews

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.user.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R .layout.activity_main);
+    }
+    public void findViews(View view){
+        EditText secret =(EditText)findViewById(R.id.secret);
+        EditText info =(EditText)findViewById(R.id.info);
+        EditText numbeer =(EditText)findViewById(R.id.number);
+        EditText send =(EditText)findViewById(R.id.send);
+        setContentView(R.layout.activity_main);
+
     }
 }
